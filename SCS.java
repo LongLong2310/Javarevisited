afd687bdec506559884834aa52828920f513405e
LongLong2310 <87378278+LongLong2310@users.noreply.github.com>
2022-10-04 13:57:12 +0700

    Add files via upload

--- SCS.java
+++ SCS.java
@@ -1,63 +1,100 @@
-// Java program to find length of
-// the shortest supersequence
-class SCS {
+class SCS
+{
 
-    // Function to find length of the
-    // shortest supersequence of X and Y.
-    static int shortestSuperSequence(String X, String Y)
+
+    // Finding the shortest common supersequence string
+    public static void shortestCommonSupersequence(String lcs, String S1, String S2)
     {
-        int m = X.length();
-        int n = Y.length();
+        String result = "";
+        int len = lcs.length();
+        int p1=0, p2=0, p3=0;
+        // for loop
+        for(int i=0; i<len; i++)
+        {
+            while(S1.charAt(p1)!= lcs.charAt(p3))
+            {
+                result += S1.charAt(p1);
+                p1++;
+            }
+            while(S2.charAt(p2)!= lcs.charAt(p3))
+            {
+                result += S2.charAt(p2);
+                p2++;
+            }
+            result += lcs.charAt(p3);
+            p1++;
+            p2++;
+            p3++;
+        }
 
-        // find lcs
-        int l = lcs(X, Y, m, n);
-
-        // Result is sum of input string
-        // lengths - length of lcs
-        return (m + n - l);
+        System.out.printf("\nSCS : %s", result);
     }
 
-    // Returns length of LCS
-    // for X[0..m - 1], Y[0..n - 1]
-    static int lcs(String X, String Y, int m, int n)
-    {
-        int[][] L = new int[m + 1][n + 1];
-        int i, j;
+    public static String lcs(String S1, String S2) {
+        int m = S1.length();
+        int n = S2.length();
 
-        // Following steps build L[m + 1][n + 1]
-        // in bottom up fashion. Note that
-        // L[i][j] contains length of LCS
-        // of X[0..i - 1]and Y[0..j - 1]
-        for (i = 0; i <= m; i++) {
-            for (j = 0; j <= n; j++) {
+        int[][] LCS_table = new int[m + 1][n + 1];
+
+        // Building the matrix
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
                 if (i == 0 || j == 0)
-                    L[i][j] = 0;
-
-                else if (X.charAt(i - 1) == Y.charAt(j - 1))
-                    L[i][j] = L[i - 1][j - 1] + 1;
-
+                    LCS_table[i][j] = 0;
+                else if (S1.charAt(i - 1) == S2.charAt(j - 1))
+                    LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1;
                 else
-                    L[i][j] = Math.max(L[i - 1][j],
-                            L[i][j - 1]);
+                    LCS_table[i][j] = Math.max(LCS_table[i - 1][j], LCS_table[i][j - 1]);
             }
         }
 
-        // L[m][n] contains length of LCS
-        // for X[0..n - 1] and Y[0..m - 1]
-        return L[m][n];
+        int index = LCS_table[m][n];
+        int temp = index;
+
+        char[] lcs = new char[index + 1];
+        lcs[index] = '\0';
+
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (S1.charAt(i - 1) == S2.charAt(j - 1)) {
+                lcs[index - 1] = S1.charAt(i - 1);
+
+                i--;
+                j--;
+                index--;
+            }
+
+            else if (LCS_table[i - 1][j] > LCS_table[i][j - 1])
+                i--;
+            else
+                j--;
+        }
+
+        // Printing the sub sequences
+        System.out.print("S1 : " + S1 + "\nS2 : " + S2 + "\nLCS: ");
+        for (int k = 0; k <= temp -1 ; k++)
+            System.out.print(lcs[k]);
+        System.out.println("");
+
+        return arr2str(lcs);
     }
 
-    // Driver code
-    public static void main(String args[])
-    {
-        String X = "AGGTAB";
-        String Y = "GXTXAYB";
+    public static String arr2str(char[] arr){
+        String result = "";
+        for (int i = 0; i < arr.length - 1; i++){
+            result += arr[i];
+        }
+        return result;
+    }
 
-        System.out.println("Length of the shortest "
-                + "supersequence is "
-                + shortestSuperSequence(X, Y));
+    public static void main(String[] args) {
+        // calling lcsAlgo()
+        String S1 = "AGGTAB";
+        String S2 = "GXTXAYB";
+
+        String S3 = lcs(S1,S2);
+
+        shortestCommonSupersequence(S3, S1,S2);
+        System.out.println();
     }
 }
-
-// This article is contributed by Sumit Ghosh
-
